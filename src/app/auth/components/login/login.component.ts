7768333f754df0e5a351ca2d173f19dd2dfcb004
cristiandev <cristiansotomayor.dev@gmail.com>
2021-02-10 23:40:45 -0500

    cpsas para login

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,12 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
+export interface ILoginForm {
+  email: string;
+  password: string;
+}
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  constructor() { }
+  loginForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.loginForm = this.createForm();
+  }
+
+  createForm(): FormGroup {
+    const obj = {
+      email: [''],
+      password: ['']
+    }
+    return this.fb.group(obj);
+  }
 
   ngOnInit(): void { }
+
+  handleLogin() {
+    const form = this.loginForm.value as ILoginForm;
+
+    console.log({form})
+  }
 }
