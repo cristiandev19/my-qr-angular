5fbbb2b52ddece395702008d3a1e8dac1ea8b34a
cristiandev <cristiansotomayor.dev@gmail.com>
2021-02-10 23:40:21 -0500

    trayendo form modules

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { MaterialModule } from '../material/material.module';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 // Aqui pondremos todos los modulos de material que usaremos
@@ -16,7 +17,9 @@ import { RegisterComponent } from './components/register/register.component';
   imports: [
     CommonModule,
     AuthRoutingModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
   ]
