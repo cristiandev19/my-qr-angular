6a5e166a88ba4ee4373f086a7ebcbabb06a629ae
cristiandev <cristiansotomayor.dev@gmail.com>
2021-02-11 23:34:12 -0500

    nuevo shared

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule
+  ],
+  providers: []
+})
+export class AppModule { }
