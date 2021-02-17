45be75b05ba0c42969f4fe1faf867d2e35420bca
cristiandev <cristiansotomayor.dev@gmail.com>
2021-02-16 21:58:09 -0500

    guard modificaciones

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -12,14 +12,19 @@ export class AuthGuard implements CanActivate {
     private router: Router
   ) {
   }
-  
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-    ): Observable<boolean> | Promise<boolean> | boolean {
+  ): Observable<boolean> | Promise<boolean> | boolean {
+
+    console.log('hola1', this.authSrv.isLoggedIn())
+    console.log('hola2', this.authSrv.haveToken())
     if (this.authSrv.isLoggedIn() && this.authSrv.haveToken()) {
       this.router.navigate(['/admin/principal']);
       // return false;
+    } else {
+      this.authSrv.logout();
     }
     return true;
   }
