73690773f302aa8d88594ccb00b78e193f08e596
cristiandev <cristiansotomayor.dev@gmail.com>
2021-02-16 21:57:57 -0500

    register ts

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,7 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-  constructor() { }
+  registerform: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.registerform = this.createForm();
+  }
+
+  createForm(): FormGroup {
+    const obj = {
+      names     : ['', [Validators.required]],
+      lastNames : ['', [Validators.required]],
+      email     : ['', [Validators.required]],
+      password  : ['', [Validators.required]]
+    }
+    return this.fb.group(obj);
+  }
 
   ngOnInit(): void { }
+
+  handleSignup() {
+  }
 }
